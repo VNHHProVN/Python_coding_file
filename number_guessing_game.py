8cc5cac31861b4329ff49cd64b8c8dd9ab1bd560
VNHHProVN <vonguyenhoaihieu2008@gmail.com>
2025-08-24 13:41:45 +0700

    Guessing number game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,16 +1,16 @@
 import random 
-number_guess = random.randint(1, 100)
+number_guess = random.randint(0, 100)
 
 while True: 
     try:
-        number_guess_input = int(input("Welcome to a guessing number game so please enter a number from 0 to 100: "))
+        number_guess_input = int(input("Chào mừng đến với trò chơi đoán số và hãy nhập một con số từ 0 đến 100: "))
         if number_guess_input < number_guess: 
-            print(f"The number {number_guess_input} is too low please enter a higher number.") 
+            print(f"Con số {number_guess_input} quá nhỏ hãy nhập một con số lớn hơn.") 
         elif number_guess_input > number_guess: 
-            print(f"The number {number_guess_input} is too high please enter a lower number.") 
+             print(f"Con số {number_guess_input} quá lớn hãy nhập một con số nhỏ hơn.") 
         else: 
-            print(f"Congratulations! The number {number_guess_input} is the guessing number.") 
+            print(f"CHÚC MỪNG! Con số {number_guess_input} chính là con số chính xác.") 
             break
     except ValueError: 
-        print("Please enter a valid number!")
+        print("Vui lòng hãy nhập một số nguyên từ 0 đến 100!")
         
