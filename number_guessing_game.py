eeda801f181a595c398cf8ef2ef44e89a2d83fc8
VNHHProVN <vonguyenhoaihieu2008@gmail.com>
2025-08-24 13:36:24 +0700

    Guessing number game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -3,13 +3,14 @@ number_guess = random.randint(1, 100)
 
 while True: 
     try:
-        number_guess_input = input("Welcome to a guessing number game so please enter a number from 0 to 100: ") 
+        number_guess_input = int(input("Welcome to a guessing number game so please enter a number from 0 to 100: "))
         if number_guess_input < number_guess: 
-            print("The number {number_guess_input} is too low please enter a higher number.") 
+            print(f"The number {number_guess_input} is too low please enter a higher number.") 
         elif number_guess_input > number_guess: 
-            print("The number {number_guess_input} is too high please enter a lower number.") 
+            print(f"The number {number_guess_input} is too high please enter a lower number.") 
         else: 
-            print("Congratulations! The number {number_guess_input} is the guessing number.")
+            print(f"Congratulations! The number {number_guess_input} is the guessing number.") 
+            break
     except ValueError: 
         print("Please enter a valid number!")
         
